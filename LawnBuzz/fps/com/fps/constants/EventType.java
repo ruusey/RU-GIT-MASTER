cb34aa8e981081c3114be68826aa76eefaf33230
rh218 <39967780+rh218@users.noreply.github.com>
2018-06-10 14:38:38 -0400

    Fixed enums

--- LawnBuzz/fps/com/fps/constants/EventType.java
+++ LawnBuzz/fps/com/fps/constants/EventType.java
@@ -4,20 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum EventType {
-	Sport(0), Concert(1), Adventure(2), Traveling(3);
-	private static Map<Integer, EventType> map = new HashMap<Integer, EventType>();
-	static {
-		for (EventType cc : EventType.values()) {
-			map.put(cc.type, cc);
-		}
-	}
-	public int type;
-
-	EventType(int type) {
-		this.type = type;
-	}
-
-	public static EventType valueOf(int type) {
-		return map.get(type);
-	}
+	Sport, Concert, Adventure, Traveling;
+	
 }
--- LawnBuzz/fps/com/fps/constants/Privledge.java
+++ LawnBuzz/fps/com/fps/constants/Privledge.java
@@ -4,20 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Privledge {
-	ReadOnly(0), Edit(1), FullAccess(2), Admin(3);
-	private static Map<Integer, Privledge> map = new HashMap<Integer, Privledge>();
-	static {
-		for (Privledge cc : Privledge.values()) {
-			map.put(cc.type, cc);
-		}
-	}
-	public int type;
-
-	Privledge(int type) {
-		this.type = type;
-	}
-
-	public static Privledge valueOf(int type) {
-		return map.get(type);
-	}
+	ReadOnly, Edit, FullAccess, Admin;
+	
 }
